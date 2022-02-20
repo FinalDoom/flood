57e5cf55b6e2ac44d2eaf178c0935737bf9ae0e4
FinalDoom <7464170-FinalDoom@users.noreply.gitlab.com>
2022-02-20 00:16:56 -0700

    Combine collapse with location filters

--- client/src/javascript/components/sidebar/LocationFilters.tsx
+++ client/src/javascript/components/sidebar/LocationFilters.tsx
@@ -1,9 +1,10 @@
-import {FC, KeyboardEvent, MouseEvent, ReactNode, TouchEvent} from 'react';
+import {FC, KeyboardEvent, MouseEvent, ReactNode, TouchEvent, useState} from 'react';
 import {observer} from 'mobx-react';
 import {useLingui} from '@lingui/react';
 import {LocationTreeNode} from '@shared/types/Taxonomy';
 
 import SidebarFilter from './SidebarFilter';
+import Expando from './Expando';
 import TorrentFilterStore from '../../stores/TorrentFilterStore';
 
 const LocationFilters: FC = observer(() => {
@@ -43,10 +44,17 @@ const LocationFilters: FC = observer(() => {
 
   const title = i18n._('filter.location.title');
 
+  const [expanded, setExpanded] = useState<boolean>(true);
+  const expandoClick = () => {
+    setExpanded(!expanded);
+  }
+
   return (
     <ul aria-label={title} className="sidebar-filter sidebar__item" role="menu">
       <li className="sidebar-filter__item sidebar-filter__item--heading" role="none">
-        {title}
+        <Expando className="sidebar-filter__item--expando" expanded={expanded} handleClick={expandoClick}>
+          {title}
+        </Expando>
       </li>
       {filterElements}
     </ul>
